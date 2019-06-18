f2a3b16645fa3f88554a2548852dd32e66dba8b5
Nate Poths <pothsn@gmail.com>
2019-06-18 11:13:27 -0500

    Initial commit after creating new repository to solve gitginore problem. First two answers are complete.

---
+++ LINQProblems/Problems.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems
+{
+    class Problems
+    {
+        public void FirstProblem()
+        {
+            //1.Using LINQ, write a function that returns all words that contain the substring “th” from a list.
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            List<string> wordsTh = words.Where(w => w.Contains("th")).ToList();
+            foreach (string word in wordsTh)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        public void SecondProblem()
+        {
+            //2.Using LINQ, write a function that takes in a list of strings and returns a copy of the list without duplicates.
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            List<string> namesUnique = names.Distinct().ToList();
+            foreach (string word in namesUnique)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ LINQProblems/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First problem test
+            //Problems problems = new Problems();
+            //problems.FirstProblem();
+            //Console.ReadLine();
+
+            //Second problem test
+            Problems problems = new Problems();
+            problems.SecondProblem();
+            Console.ReadLine();
+
+        }
+    }
+}
